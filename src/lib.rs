ae88243917e34c6e690a6097863daa778158eb9a
gyu-don <takumi.kt+git@gmail.com>
2020-09-13 19:11:05 +0900

    Debugging syndrome measurement

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ const MEASURE_ANCILLA_QUBITS: Qubit = 6;
 const MEASURE_MASK: u32 = 127;
 
 pub struct SteaneLayer {
-    sim: GottesmanKnillSimulator<DefaultRng>,
+    // TODO: not pub.
+    pub sim: GottesmanKnillSimulator<DefaultRng>,
     n_logical_qubits: Qubit
 }
 
@@ -26,60 +27,64 @@ impl SteaneLayer {
     }
 
     fn syndrome_measure_and_recover(&mut self) {
+        eprintln!("START syndrome_measure_and_recover");
         let m0 = self.measure_ancilla();
-        for i in 0..m0 {
-            self.sim.h(i);
-        }
         for i in 0..self.n_logical_qubits {
             for j in i * PHISQUBIT_PER_LOGQUBIT..(i + 1) * PHISQUBIT_PER_LOGQUBIT {
                 self.sim.h(j);
             }
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT, m0);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 1);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 2);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 1);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 2);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0 + 2);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 1);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 2);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 1);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT, m0);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 1);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 2);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 1);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 2);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0 + 2);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 1);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 2);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 1);
             for j in i * PHISQUBIT_PER_LOGQUBIT..(i + 1) * PHISQUBIT_PER_LOGQUBIT {
                 self.sim.h(j);
             }
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT, m0 + 3);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT, m0 + 5);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 4);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 5);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 4);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 5);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 6);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 4);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0 + 5);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 6);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 4);
-            self.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 5);
-            for j in 0..PHISQUBIT_PER_LOGQUBIT {
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT, m0 + 3);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT, m0 + 5);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 4);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 1, m0 + 5);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 3);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 4);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 2, m0 + 5);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 3, m0 + 3);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 4, m0 + 4);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 5, m0 + 5);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 3);
+            self.sim.cx(i * PHISQUBIT_PER_LOGQUBIT + 6, m0 + 4);
+            for j in 0..MEASURE_ANCILLA_QUBITS {
                 self.sim.measure(i * PHISQUBIT_PER_LOGQUBIT + j, j);
             }
+            // TODO: バックエンド変わったときダメ。まともな方法
             let measured = self.sim.measured.inner[0] & MEASURE_MASK;
+            self.sim.measured.inner[0] = 0;
+            eprintln!("logical qubit: {}, measured: {:b}", i, measured);
             for j in 0..PHISQUBIT_PER_LOGQUBIT {
                 // reset
                 if measured & (1 << j) != 0 {
-                    self.x(i * PHISQUBIT_PER_LOGQUBIT + j);
+                    self.sim.x(i * PHISQUBIT_PER_LOGQUBIT + j);
                 }
             }
             if measured & 7 > 0 {
-                let err_x = ERR_TABLE_X[(measured & 7) as usize];
-                self.x(err_x);
+                let err_x = ERR_TABLE_X[(measured & 7) as usize] + i * PHISQUBIT_PER_LOGQUBIT;
+                eprintln!("X Err on {}", err_x);
+                self.sim.x(err_x);
             }
             if (measured >> 3) > 0 {
-                let err_z = ERR_TABLE_X[(measured >> 3) as usize];
-                self.z(err_z);
+                let err_z = ERR_TABLE_X[(measured >> 3) as usize] + i * PHISQUBIT_PER_LOGQUBIT;
+                eprintln!("Z Err on {}", err_z);
+                self.sim.z(err_z);
             }
         }
+        eprintln!("END   syndrome_measure_and_recover");
     }
 }
 
@@ -88,6 +93,7 @@ impl Operations for SteaneLayer {
     type Slot = Qubit;
     fn initialize(&mut self) {
         self.sim.initialize();
+        //self.syndrome_measure_and_recover();
     }
     fn measure(&mut self, q: <Self as lay::Operations>::Qubit, c: <Self as lay::Operations>::Slot) {
 
@@ -135,8 +141,41 @@ impl CliffordGate for SteaneLayer {
 
 #[cfg(test)]
 mod tests {
+    use lay::Operations;
+    use lay::gates::{CliffordGate};
+    use lay_simulator_gk::{ GottesmanKnillSimulator, DefaultRng, Qubit };
+    use crate::SteaneLayer;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_works2() {
+        let mut steane = SteaneLayer::new(3);
+        steane.initialize();
+        eprintln!("First syndrome measurement 5 times");
+        steane.syndrome_measure_and_recover();
+        steane.syndrome_measure_and_recover();
+        steane.syndrome_measure_and_recover();
+        steane.syndrome_measure_and_recover();
+        steane.syndrome_measure_and_recover();
+        eprintln!("END First syndrome measurement 5 times");
+        eprintln!("Expected: not shown");
+        steane.syndrome_measure_and_recover();
+        steane.x(0);
+        eprintln!("Expected: not shown");
+        steane.syndrome_measure_and_recover();
+        steane.sim.x(12);
+        eprintln!("Expected: 12");
+        steane.syndrome_measure_and_recover();
+        eprintln!("Expected: not shown");
+        steane.syndrome_measure_and_recover();
+        steane.sim.z(8);
+        eprintln!("Expected: 8");
+        steane.syndrome_measure_and_recover();
+        eprintln!("Expected: not shown");
+        steane.syndrome_measure_and_recover();
     }
 }
